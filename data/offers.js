47931969958cc5f0f7e22632c20b0a6397418f1b
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2024-11-11 14:28:27 +0100

    fdf

--- data/offers.js
+++ data/offers.js
@@ -14,19 +14,19 @@ export const offerBoxes = [
     icon: "/assets/img/icon/offering1_3.png",
     delay: ".9s",
   },
-  {
-    title: "Watch",
-    icon: "/assets/img/icon/offering1_4.png",
-    delay: "1s",
-  },
-  {
-    title: "Tv",
-    icon: "/assets/img/icon/offering1_5.png",
-    delay: "1.3s",
-  },
-  {
-    title: "IOT",
-    icon: "/assets/img/icon/offering1_6.png",
-    delay: "1.6s",
-  },
+  // {
+  //   title: "Watch",
+  //   icon: "/assets/img/icon/offering1_4.png",
+  //   delay: "1s",
+  // },
+  // {
+  //   title: "Tv",
+  //   icon: "/assets/img/icon/offering1_5.png",
+  //   delay: "1.3s",
+  // },
+  // {
+  //   title: "IOT",
+  //   icon: "/assets/img/icon/offering1_6.png",
+  //   delay: "1.6s",
+  // },
 ];
