43d6198d2c9813b125db88368d6db93853d64f09
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2024-11-12 11:50:10 +0100

    fdf

--- data/facts.js
+++ data/facts.js
@@ -1,25 +1,25 @@
 export const counterBoxes = [
   {
     icon: "/assets/img/icon/counterIcon1_1.png",
-    number: 6561,
+    number: 561,
     label: "Satisfied Clients",
     delay: ".3s",
   },
   {
     icon: "/assets/img/icon/counterIcon1_2.png",
-    number: 600,
+    number: 60,
     label: "Finished Projects",
     delay: ".6s",
   },
   {
     icon: "/assets/img/icon/counterIcon1_1.png",
-    number: 250,
+    number: 20,
     label: "Skilled Experts",
     delay: ".9s",
   },
   {
     icon: "/assets/img/icon/counterIcon1_1.png",
-    number: 5900,
+    number: 590,
     label: "Media Posts",
     delay: "1s",
   },
