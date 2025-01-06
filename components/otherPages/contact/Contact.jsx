bc7c7aba32ba625869ff1c35351acf8ac702f583
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2025-01-06 14:18:31 +0100

    fd

--- components/otherPages/contact/Contact.jsx
+++ components/otherPages/contact/Contact.jsx
@@ -269,7 +269,7 @@ export default function Contact() {
         channel="youtube"
         youtube={{ mute: 0, autoplay: 0 }}
         isOpen={isOpen}
-        videoId="f2Gzr8sAGB8"
+        videoId="ZUyyDk4GdVI"
         onClose={() => setOpen(false)}
       />
     </>
